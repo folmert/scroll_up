20d983726dc4f485f309c297e7c4b6e0d9d96017
folmert <folmert2@gmail.com>
2015-07-26 13:23:08 +0200

    initial commit

---
+++ js/scroll_up.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var scroll_up_btn = document.createElement('div');
+scroll_up_btn.classList.add('scroll_up');
+document.body.appendChild(scroll_up_btn);
+
+window.addEventListener('scroll', function() {
+
+	if (window.pageYOffset > 300) {
+		scroll_up_btn.style.display = 'block';
+	}
+	else {
+		scroll_up_btn.style.display = 'none';
+	}
+
+});
+
+
+scroll_up_btn.addEventListener('click', function(e) {
+	
+	// jQuery("html, body").animate({
+		// 	scrollTop: 0
+		// }, {
+			// 	easing: 'swing',
+			// 	complete: function() {
+				// 		jQuery('.scrollUp').hide();
+
+				// 	}
+				// });
+
+window.scrollTo(0, 0);
+
+
+},false);
